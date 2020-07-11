f983689a413e6e638b0e3fb19dd01bb2a343a3f3
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-07-11 00:45:22 -0500

    feat: import feed, sidebar, and header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import Feed from "./Feed";
 
 function App() {
-  return <div className="font-body">hey</div>;
+  return (
+    <div className="font-body flex fixed h-full w-full">
+      <Sidebar />
+      <div className="bg-gray-lightest h-full w-full px-12">
+        <Header />
+        <Feed />
+      </div>
+    </div>
+  );
 }
 
 export default App;
