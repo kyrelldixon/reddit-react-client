1ca52f8b36cf2601ff1880b87b89ccf4d994e10b
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-07-10 23:48:01 -0500

    chore(tailwind): update height and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,18 @@ module.exports = {
     "public/**/*.html",
   ],
   theme: {
+    extend: {
+      height: {
+        "1px": "1px",
+        "2px": "2px",
+      },
+    },
     colors: {
       orange: "#fd4500",
       gray: {
         lightest: "#fbfcfe",
         lighter: "#f4f5fa",
-        light: "#898ea2",
+        light: "#e5e5ed",
         normal: "#808080",
         dark: "#464855",
       },
