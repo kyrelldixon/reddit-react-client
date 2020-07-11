3ed02259cee5ff65697e4848227232e42197fcea
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-07-11 12:05:06 -0500

    feat: make header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { FaReddit } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex justify-between py-8">
-      <ul className="flex space-x-8 text-gray-normal font-heading">
+    <header className="flex flex-col md:flex-row-reverse justify-between py-8 font-heading md:items-center">
+      <div className="flex justify-between items-center">
+        <div className="flex md:hidden justify-center items-center mr-2">
+          <FaReddit className="text-orange text-3xl mr-2" />
+          <span className="text-2xl">Reddit</span>
+        </div>
+        <p className="text-gray-dark">Username</p>
+      </div>
+      <ul className="flex w-full justify-between max-w-md text-sm md:text-base text-gray-normal justify-center pt-6 pb-2 md:py-0 mx-auto md:mx-0">
         <li className="text-orange">
           <span>Popular</span>
           <div className="w-full h-2px bg-orange transform translate-y-2"></div>
@@ -14,9 +22,6 @@ export default function Header() {
         <li>Top</li>
         <li>Gilded</li>
       </ul>
-      <div>
-        <p>Username</p>
-      </div>
     </header>
   );
 }
